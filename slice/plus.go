4b3a6b8163bae7913e23a944baa9a4e7c06460d4
Jaehyun Yeom <jae.yeom@gmail.com>
2016-07-08 08:45:47 +0000

    Reimplemented PlusStrings using append variadic.

--- slice/plus.go
+++ slice/plus.go
@@ -1,22 +1,9 @@
 package slice
 
-// PlusStrings adds two []string arrays together, returning a new []string.
+// PlusStrings adds two []string arrays together, returning a new copied []string.
 func PlusStrings(s, plus []string) []string {
 
-	var l int
-
-	a := []string{}
-
-	l = len(s)
-	for i := 0; i < l; i++ {
-		a = append(a, s[i])
-	}
-
-	l = len(plus)
-	for i := 0; i < l; i++ {
-		a = append(a, plus[i])
-	}
-
-	return a
+	a := make([]string, 0, len(s)+len(plus))
+	return append(append(a, s...), plus...)
 
 }
--- slice/plus_test.go
+++ slice/plus_test.go
@@ -1,8 +1,9 @@
 package slice
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPlusStrings(t *testing.T) {
@@ -17,6 +18,53 @@ func TestPlusStrings(t *testing.T) {
 		assert.Equal(t, "two", all[1])
 		assert.Equal(t, "three", all[2])
 		assert.Equal(t, "four", all[3])
+	}
+
+}
+
+func TestPlusStrings_ensureCopy(t *testing.T) {
+
+	s1 := []string{"one", "two", "three", "four", "five"}
+	s2 := []string{"six", "seven"}
+
+	all := PlusStrings(s1[:2], s2)
+	s1[0] = "eight"
+
+	if assert.Equal(t, 4, len(all)) {
+		assert.Equal(t, "one", all[0])
+		assert.Equal(t, "two", all[1])
+		assert.Equal(t, "six", all[2])
+		assert.Equal(t, "seven", all[3])
+	}
+
+	if assert.Equal(t, 5, len(s1)) {
+		assert.Equal(t, "eight", s1[0])
+		assert.Equal(t, "two", s1[1])
+		assert.Equal(t, "three", s1[2])
+		assert.Equal(t, "four", s1[3])
+		assert.Equal(t, "five", s1[4])
+	}
+
+}
+
+func TestPlusStrings_ensureCopyNilPlus(t *testing.T) {
+
+	s1 := []string{"one", "two", "three", "four", "five"}
+
+	all := PlusStrings(s1[:2], nil)
+	s1[0] = "eight"
+
+	if assert.Equal(t, 2, len(all)) {
+		assert.Equal(t, "one", all[0])
+		assert.Equal(t, "two", all[1])
+	}
+
+	if assert.Equal(t, 5, len(s1)) {
+		assert.Equal(t, "eight", s1[0])
+		assert.Equal(t, "two", s1[1])
+		assert.Equal(t, "three", s1[2])
+		assert.Equal(t, "four", s1[3])
+		assert.Equal(t, "five", s1[4])
 	}
 
 }
